c6a75eda6f6abdf988222849026596ac67c64075
Mohit <mohit.parashar@bdiplus>
2024-04-22 23:35:24 +0530

    issue resolved

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,8 +37,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        service_hu5azpm,
+        template_9eoi99d,
         {
           from_name: form.name,
           to_name: "Mohit Parashar",
@@ -46,7 +46,7 @@ const Contact = () => {
           to_email: "jindal8077@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        f-Zm4AHdYzvv_sdvt
       )
       .then(
         () => {
